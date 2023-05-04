1c6ca8605496608bbf4202f190361aab45f2b19f
etsi123 <etsi@seas.upenn.edu>
2023-05-03 21:59:42 -0400

    Clean up code, convert to class.

---
+++ trading_strats.py
@@ -0,0 +1,68 @@
+#Construct a class to simulate an average down strategy. 
+import pandas as pd
+import numpy as np
+import yfinance as yf
+import datetime
+from datetime import date, timedelta
+
+class stock_data:
+    def __init__(self,ticker,num_days,num_chunks,percent_cutoff): 
+        self.ticker = ticker
+        self.num_days = num_days
+        self.num_chunks = num_chunks
+        self.percent_cutoff = percent_cutoff
+    def get_stock_data(self): 
+        today = date.today()
+        d1 = today.strftime("%Y-%m-%d")
+        end_date = d1
+        d2 = date.today() - timedelta(days=self.num_days)
+        d2 = d2.strftime("%Y-%m-%d")
+        start_date = d2
+
+        data = yf.download(self.ticker,start=start_date,end=end_date, progress=False).reset_index()
+        data['percent_change'] = (data['Close']-data['Open'])/data['Close']*100    
+        self.data = data
+        return self.data
+    def simulate_avg_down_strategy(self): 
+        total_investable_amount_og = 10000
+        total_investable_amount = total_investable_amount_og
+        num_chunks = self.num_chunks
+        dollar_chunks = total_investable_amount / num_chunks
+        total_investable_amount = total_investable_amount - dollar_chunks 
+
+        percent_change_list = self.data.percent_change.tolist()
+        close_list = self.data.Close.tolist()
+        open_list = self.data.Open.tolist()
+
+        initial_price = close_list.pop(0)
+        initial_num_stocks = dollar_chunks / initial_price
+        avg_price = initial_price
+        num_stocks = initial_num_stocks
+        percent_change_list.pop(0)
+        open_list.pop(0)
+        
+        for i in range(0,len(percent_change_list)): 
+            #Market Opens
+            if avg_price >= open_list[i] and num_stocks > initial_num_stocks: 
+                total_investable_amount = total_investable_amount + (num_stocks - initial_num_stocks)*(avg_price)
+                num_stocks = initial_num_stocks
+            #Market Closes    
+            percent_change = percent_change_list[i]
+            if percent_change < self.percent_cutoff and close_list[i] < avg_price and total_investable_amount >= total_investable_amount_og/num_chunks: 
+                purchase_price = close_list[i]
+                num_purchased_today = dollar_chunks / purchase_price
+                total_investable_amount = total_investable_amount - dollar_chunks
+                avg_price = (num_stocks*avg_price + num_purchased_today*purchase_price) / (num_stocks + num_purchased_today)
+                num_stocks = num_stocks + num_purchased_today
+
+
+        profit_dollar = num_stocks*(close_list[-1]-avg_price)
+        profit_percentage = np.round(profit_dollar / total_investable_amount_og *100,2)
+
+        strategy_percent_increase = (total_investable_amount + num_stocks* close_list[-1] - total_investable_amount_og)*100/total_investable_amount_og
+        strategy_percent_increase = np.round(strategy_percent_increase,2)
+        buy_and_hold_increase = np.round(((close_list[-1] - initial_price)/initial_price)*100,2)
+        
+        print('For ticker ' + str(self.ticker) + ' and duration = ' + str(self.num_days) + ' days:')
+        print('My strat. yields a ' + str(strategy_percent_increase) + ' % increase. ')
+        print('Buy and hold yields a ' + str(buy_and_hold_increase) + str(' % increase')) 
